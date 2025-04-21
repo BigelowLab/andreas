ca0fa63b36629d90aba264d2beeb5082bf5dbd44
Ben Tupper <btupper@bigelow.org>
2025-04-21 09:54:48 -0400

    move database.R and lut.R from copernicus

---
+++ R/andrew-package.R
@@ -0,0 +1,11 @@
+#' andrew 
+#'
+#' Andrew (Andreas) Copernicus was Nicolaus Copernicus' lesser known brother.
+#' 
+#' @name andrew-package
+#' @aliases andrew
+#' @importFrom utils glob2rx
+#' @importFrom rlang .data
+#' @description Serves Center for Ocean Forecasting Copernicus needs.
+#' @keywords internal 
+"_PACKAGE"
