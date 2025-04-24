afe0a479803cfca3b7c3ee4f1dcf16e74427a79f
Ben Tupper <btupper@bigelow.org>
2025-04-24 18:38:38 -0400

    many changes to dynamic lut system, add fetch_andreas, amend README

---
+++ R/fetch.R
@@ -0,0 +1,160 @@
+#' A wrapper around [copernicus::fetch_copernicus()] to fetch Copernicus data as
+#' stars objects.
+#' 
+#' @export
+#' @param x table - a look up table with one or more datasets
+#' @param drop_depth logical, if TRUE then drop the depth dimension
+#' @param ... other arguments passed to [copernicus::fetch_copernicus()]
+#' @return stars object
+fetch_andreas = function(x, 
+                         drop_depth = TRUE,
+                         ...){
+  
+  r = x |>
+    dplyr::group_by(.data$dataset_id, .data$depth) |>
+    dplyr::group_map(
+      function(tbl, key, drop_depth = TRUE){
+        filename = tempfile(fileext = ".nc")
+        depth = if(is.na(tbl$mindepth[1])) {
+            NULL
+          } else {
+            c(tbl$mindepth[1], tbl$maxdepth[1])
+          }
+        s = copernicus::fetch_copernicus(dataset_id = tbl$dataset_id[1], 
+                                         vars = dplyr::pull(tbl, dplyr::all_of("short_name")),
+                                         depth = depth,
+                                         ofile = filename,
+                                         ...) |>
+              rlang::set_names(dplyr::pull(tbl, dplyr::all_of("short_name")))
+        if (drop_depth) s = dplyr::slice(s, "depth", 1)
+        s
+      }, .keep = TRUE, drop_depth = drop_depth) |>
+    copernicus::bind_stars()
+  r
+}
+
+#' A convenience tool for fetching a product suite on a given day.
+#' 
+#' This only works for known products/datasets.  Unknowns will generate a
+#' warning and return NULL. For each dataset only a single depth layer is 
+#' returned. 
+#' 
+#' @export
+#' @param p table of product info to be grouped by `dataset_id`
+#' @param x Date (or YYYY-mm-dd string)
+#' @param bb bounding box or somehting from which a bounding box can be found.
+#' @param ... other arguments for [fetch_copernicus_cli_subset]
+#' @return list of stars objects (one for each dataset) possibly with NULLs for unknown datasets
+fetch_product_by_day = function(p = product_lut() |>
+                                  dplyr::filter(.data$fetch == "yes",
+                                                .data$product_id == .data$product_id[1]),
+                                x = Sys.Date()-7,
+                                bb = c(xmin = -180, ymin = -90, 
+                                       xmax = 180, ymax = 90) |>
+                                  sf::st_bbox(crs = 4326),
+                                ...){
+  dates = c(x[1], x[length(x)])
+
+  p = p |>
+    dplyr::group_by(.data$dataset_id)
+  
+  p |>
+    dplyr::group_map(
+      function(tbl, key){
+        switch(key$dataset_id,
+               "cmems_mod_glo_phy_anfc_0.083deg_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = NULL,
+                                             ...))
+                 if (inherits(s, "try-error")){
+                   s = NULL
+                 }
+                 s
+               },
+               "cmems_mod_glo_phy_anfc_0.083deg-sst-anomaly_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = NULL,
+                                             ...))
+                 if (inherits(s, "try-error")){
+                   s = NULL
+                 }
+                 if (!is.null(s)) s = s |> rlang::set_names("sstanom")
+                 s
+               },
+               "cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = c(tbl$mindepth[1],tbl$maxdepth[1]),
+                                             ...))
+                 if (inherits(s, "try-error")){
+                   s = NULL
+                 }
+                 if (!is.null(s)) s = s |> dplyr::slice("depth", 1)
+                 s
+               },
+               "cmems_mod_glo_phy-so_anfc_0.083deg_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = c(tbl$mindepth[1],tbl$maxdepth[1]),
+                                             ...))
+                 if (inherits(s, "try-error")) s = NULL
+                 
+                 if (!is.null(s)) s = s |> dplyr::slice("depth", 1)
+                 s
+               },
+               "cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = c(tbl$mindepth[1],tbl$maxdepth[1]),
+                                             ...))
+                 if (inherits(s, "try-error")) s = NULL
+                 if (!is.null(s)) s = s |> dplyr::slice("depth", 1)
+                 s
+               },
+               "cmems_mod_glo_phy-wcur_anfc_0.083deg_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = c(tbl$mindepth[1],tbl$maxdepth[1]),
+                                             ...))
+                 if (inherits(s, "try-error")) s = NULL
+                 if (!is.null(s)) s = s |> dplyr::slice("depth", 1)
+                 s
+               },
+               "cmems_mod_glo_phy_my_0.083deg_P1D-m" = {
+                 s = try(fetch_copernicus_cli_subset(dataset_id = key$dataset_id,
+                                             product = tbl$product_id[1],
+                                             vars = tbl$short_name,
+                                             time = dates,
+                                             bb = bb,
+                                             depth = c(tbl$mindepth[1],tbl$maxdepth[1]),
+                                             ...))
+                 if (inherits(s, "try-error")) s = NULL
+                 s
+               },
+               {
+                 warning("dataset_id not known, contact developer:", key$dataset_id)
+                 NULL
+               })
+      }) |>
+    rlang::set_names(dplyr::group_data(p) |> dplyr::pull(1))
+}
--- R/lut.R
+++ R/lut.R
@@ -1,50 +1,103 @@
-#' Read a product LUT
+#' Read a product LUT used for searches and merge with a product description
 #' 
 #' @export
-#' @param product_id char, the product identifier
-#' @param add_meta logical, if TRUE merge with `dataset_metadata`
-#' @return tibble
-product_lut = function(product_id = 'GLOBAL_ANALYSISFORECAST_PHY_001_024',
-                       add_meta = TRUE){
+#' @param product_id chr, the product identifier.  The associated file must exist
+#' @param description table, the table produced by [copernicus::read_product_description()] for
+#'   the specified `product_id`
+#' @return a table with the following
+#' * dataset_id
+#' * dataset_name
+#' * name chr short_name converted to camelCase (used for file View(xstorage)
+#' * short_name
+#' * standard_name
+#' * units 
+#' * depth really a depth name "sur", "bot", "mld" or NA but could be a number (as character())
+#' * fetch   "yes", "no" or NA
+#' * mindepth for `copernicusmarine` CLI request
+#' * maxdepth for `copernicusmarine` CLI request
+#' * start_time known data service availability
+#' * end_time known data service availability
+read_product_lut = function(product_id = 'GLOBAL_ANALYSISFORECAST_PHY_001_024',
+                            description = copernicus::read_product_description(product_id)){
   
   filename = copernicus_path("lut", paste0(product_id[1], ".csv"))
-  x = readr::read_csv(filename, show_col_types = FALSE) 
-  if ("short_name" %in% colnames(x)){
-    ix = x$short_name == "sea_surface_temperature_anomaly"
-    x$short_name[ix] = "sstanom"
-  }
+  if(!file.exists(filename)) stop("product lut file not found: ", filename)
+  lut = readr::read_csv(filename, show_col_types = FALSE) |> 
+    dplyr::mutate(name = snakecase::to_lower_camel_case(.data$short_name),
+                  .before = dplyr::all_of("short_name"))
   
-  if (add_meta && ("short_name" %in% colnames(x))){
-    m = product_lut("dataset_metadata") |>
-      dplyr::select(-dplyr::all_of("product_id"))
-    x = x |>
-      dplyr::left_join(m, by = "dataset_id")
-  }
+  lut |>
+    dplyr::select(-dplyr::all_of(c("product_id", "title"))) |>
+    dplyr::full_join(description |>
+                       dplyr::select(-dplyr::all_of(c("time_step", "min_depth", "max_depth", "units",
+                                                      "standard_name"))) |>
+                       dplyr::filter(.data$dataset_id %in% lut$dataset_id & .data$short_name %in% lut$short_name), 
+                     by = c("dataset_id", "short_name")) 
   
-  x
 }
 
-#' Create the table of dataset metadata
+#  Read a product LUT
+#  
+#  @export
+#  @param product_id char, the product identifier
+#  @param add_meta logical, if TRUE merge with `dataset_metadata`
+#  @return tibble
+# product_lut = function(product_id = 'GLOBAL_ANALYSISFORECAST_PHY_001_024',
+#                        add_meta = TRUE){
+#   
+#   filename = copernicus_path("lut", paste0(product_id[1], ".csv"))
+#   x = readr::read_csv(filename, show_col_types = FALSE) 
+#   if ("short_name" %in% colnames(x)){
+#     ix = x$short_name == "sea_surface_temperature_anomaly"
+#     x$short_name[ix] = "sstanom"
+#   }
+#   
+#   if (add_meta && ("short_name" %in% colnames(x))){
+#     m = product_lut("dataset_metadata") |>
+#       dplyr::select(-dplyr::all_of("product_id"))
+#     x = x |>
+#       dplyr::left_join(m, by = "dataset_id")
+#   }
+#   
+#   x
+# }
+
+#  #' Create the table of dataset metadata
+#  #' 
+#  #' @export
+#  #' @param x catalog (flattened or unflattened)
+#  #' @return table narrowed and depduplictaed
+#  create_dataset_metadata = function(x = read_product_catalog()){
+#    x |> 
+#      dplyr::select(dplyr::all_of(c("product_id", "dataset_id"))) |>
+#      dplyr::distinct() |>
+#      dplyr::mutate(start_date = NA,
+#                    end_date = NA)
+#  }
+
+#' Generate a LUT suitable for the package.
 #' 
-#' @export
-#' @param x catalog (flattened or unflattened)
-#' @return table narrowed and depduplictaed
-create_dataset_metadata = function(x = read_product_catalog()){
-  x |> 
-    dplyr::select(dplyr::all_of(c("product_id", "dataset_id"))) |>
-    dplyr::distinct() |>
-    dplyr::mutate(start_date = NA,
-                  end_date = NA)
-}
-
-#' Generate a LUT suitable for the package
+#' If you chose to save (see `save_lut`) then be advised existing an existing
+#' lut will be overwritten.
 #' 
 #' @export
 #' @param x chr the name of the product
 #' @param catalog table of products (unflattened)
 #' @param save_lut log, if TRUE save to CSV format in `inst/lut`
-#' @return a table of look up values.  You'll edit this file to decide whihc to fetch
-#'  and what depths to fetch from.  
+#' @return a table of look up values.  You'll edit this file to decide which to fetch
+#'  and what depths to fetch from.  Here's what is in the output table
+#'  
+#' * product_id
+#' * title
+#' * dataset_id
+#' * dataset_name
+#' * short_name
+#' * standard_name
+#' * units
+#' * depth "sur"
+#' * fetch  "no"
+#' * mindepth 0
+#' * maxdepth 1
 create_lut <- function(x = "GLOBAL_ANALYSISFORECAST_BGC_001_028",
                        catalog = read_product_catalog(),
                        save_lut = FALSE){
