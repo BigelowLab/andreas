9e1180cf51682fc34308212421251e459f0b665f
Ben Tupper <btupper@bigelow.org>
2025-12-02 13:54:35 -0500

    add variables to variable_lut()

--- R/misc.R
+++ R/misc.R
@@ -52,7 +52,15 @@ variable_lut = function(){
     "uo", "uo", "eastward current velocity", "(m/s)",
     "vo", "vo", "northward current velocity", "(m/s)",
     "wo", "wo", "upward current velocity", "(m/s)",
-    "zos", "zos", "sea surface height", "(m)")
+    "zos", "zos", "sea surface height", "(m)",
+    "chl", "chl", "chlorophyll", "mg m-3",
+    "no3", "no3", "nitrate", "mmol m-3",
+    "nppv", "nppv", "net primary productionas carbon", "mg m-3 day-1",
+    "o2", "o2", "oxygen", "mmol m-3",
+    "po4", "po4", "phosphate", "mmol m-3",
+    "si", "si", "silcate", "mmol m-3",
+    "zooc", "zooc", "total zooplankton as carbon", "mmol m-3",
+    "phyc", "phyc", "total phytoplankton as carbon", "mmol m-3")
 }
 
 #' Retrieve the common name of a given variable name
