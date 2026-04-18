f7c9165dcd1f6bdac7d90a8c76592ad2385ab54c
Ben Tupper <btupper@bigelow.org>
2026-04-18 09:58:20 -0400

    import bind_attrs and bind_stars from copernicus

--- R/reexport-copernicus.R
+++ R/reexport-copernicus.R
@@ -9,3 +9,11 @@ copernicus::make_path
 #' @importFrom copernicus set_point
 #' @export
 copernicus::set_point
+
+#' @importFrom copernicus bind_attrs
+#' @export
+copernicus::bind_attrs
+
+#' @importFrom copernicus bind_stars
+#' @export
+copernicus::bind_stars
